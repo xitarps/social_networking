1a2bd9742ee3600e314647347963f14d06dad3d9
xita Rps <xitarps@gmail.com>
2021-05-25 07:46:16 -0300

    Add user register

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users
-  # devise_for :admins, skip: [:registrations], controllers: {sessions: 'admins/sessions'}
+  devise_for :users, controllers: { registrations: 'users/registrations'}
   devise_for :admins, skip: [:registrations], controllers: { sessions: 'admin/sessions' }
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
@@ -9,6 +8,7 @@ Rails.application.routes.draw do
   namespace :admin do
     root to: "home#index"
     resources :admins
+    resources :users, only: %i[index]
   end
 
   namespace :user do
